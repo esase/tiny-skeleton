0b735b7181cca54723daccb654808cfd3b498998
Alex Ermashev <alexermashev@medrecord.io>
2020-08-17 17:01:52 +0600

    add unit tests for the 'View'

--- test/ViewTest.php
+++ test/ViewTest.php
@@ -13,9 +13,86 @@ namespace Tiny\Skeleton;
 
 use PHPUnit\Framework\TestCase;
 use Tiny\Skeleton\Module\Core\Exception;
+use Tiny\EventManager;
 
 class ViewTest extends TestCase
 {
+
+    public function test__callMethodUsingNotInitializedEventManager()
+    {
+        $method = 'test';
+
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            sprintf(
+                'The method "%s()" is unsupported.',
+                $method
+            )
+        );
+
+        $view = new View();
+
+        $view->{$method}();
+    }
+
+    public function test__callMethodUsingNotRegisteredHelper()
+    {
+        $method = 'test';
+
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $this->expectExceptionMessage(
+            sprintf(
+                'The method "%s()" is unsupported.',
+                $method
+            )
+        );
+
+        $view = new View();
+        $view->setEventManager(
+            $this->createStub(EventManager\EventManager::class)
+        );
+
+        $view->{$method}();
+    }
+
+    public function test__callMethod()
+    {
+        $method = 'test';
+
+        $eventManagerMock = $this->createMock(EventManager\EventManager::class);
+        $eventManagerMock->expects($this->once())
+            ->method('isEventHasSubscribers')
+            ->with(
+                'view.call.helper.test'
+            )
+            ->willReturn(true);
+        $eventManagerMock->expects($this->once())
+            ->method('trigger')
+            ->with(
+                'view.call.helper.test',
+                $this->isInstanceOf(EventManager\Event::class)
+            )
+            ->will(
+                $this->returnCallback(
+                    function (string $eventName, EventManager\Event $event) {
+                        // make sure that event is stopped for the rest of subscribers
+                        $this->assertTrue($event->isStopped());
+
+                        // modify the event's data
+                        $event->setData('test content');
+                    }
+                )
+            );
+
+        $view = new View();
+        $view->setEventManager(
+            $eventManagerMock
+        );
+
+        $result = $view->{$method}();
+
+        $this->assertEquals('test content', $result);
+    }
 
     public function test__getMethod()
     {
@@ -42,7 +119,9 @@ class ViewTest extends TestCase
                 'test' => 'test_value',
             ]
         );
-        $view->setTemplatePath(__DIR__.'/fixtures/templates/test_template.phtml');
+        $view->setTemplatePath(
+            __DIR__.'/fixtures/templates/test_template.phtml'
+        );
         $view->setLayoutPath(__DIR__.'/fixtures/templates/test_layout.phtml');
 
         $this->assertEquals(
@@ -71,7 +150,9 @@ class ViewTest extends TestCase
                 'test' => 'test_value',
             ]
         );
-        $view->setTemplatePath(__DIR__.'/fixtures/templates/test_template.phtml');
+        $view->setTemplatePath(
+            __DIR__.'/fixtures/templates/test_template.phtml'
+        );
 
         $content = $view->__toString();
 
